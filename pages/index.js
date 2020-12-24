fe88f917372fbcf290c8db0ae1054dbc0bf691aa
Nicole Sevillano <34521909+s3vi26@users.noreply.github.com>
2020-12-23 17:06:35 -0800

    initial

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Sevi Codes</title>
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
       <main className={styles.main}>
